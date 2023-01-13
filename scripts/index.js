98bf7304f4e4247fe8cc387de78e8db9bbbfec39
Katherine de Vay <katdevay2@gmail.com>
2023-01-13 12:12:01 -0600

    Draft populate function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,4 @@
-// 1. Ship factory function
-//     - creates ship objects with properties for length, hitCount, and isSunk (bool).
-//     - only test methods or properties used outside of ship object (aka only public interface elements)
-//     - hit() function increments hitCount
-//     - isSunk() returns T/F based on length and hitCount
-
+// 1. Creates ship objects with properties for length, hitCount, hit, and isSunk.
 function ship(length){
     return {
         length: length,
@@ -12,5 +7,57 @@ function ship(length){
         isSunk() { return this.length - this.hitCount > 0 ? false : true }
     }
 }
-
 module.exports = ship;
+ 
+/*
+
+*/
+// 2. Creates and populates board with ships, and tracks/displays missed attacks
+const gameBoard = {
+    ships: [ship(5), ship(4), ship(4), ship(3), ship(3), ship(3), ship(2), ship(2), ship(2), ship(2)],
+    tiles: [],
+    createTiles() {
+        for (let i = 0; i < 10; i++) {
+            let temp = [];
+            for (let j = 0; j < 10; j++) {
+                temp.push({isFilled: false, ship: null;});
+        }
+        this.tiles.push(temp);
+        }
+        return;
+    },
+    isFilled(row, col){
+        return tile[row][col].isFilled ? true : false;
+    },
+    randomDirection(){
+        return (Math.random() * (11 - 0) + 0) <= 5 ? 'h' : 'v';
+    },
+    randomCoordinates(length) {
+        let a = Math.round(Math.random() * (11 - 0) + 0);
+        let b =  Math.round(Math.random() * (11 - 0) + 0);
+        let arr = []
+        if (this.isFilled(a, b)){
+            return this.randomCoordinates();
+        }
+        let dir = this.randomDirection();
+        for (i = 0; i < length; i++){
+            dir === 'h' ? arr.push(a, b + i) : arr.push(a + i, b);
+        }
+        for (i = 0; i < length * 2; i + 2){
+            if (this.isFilled(arr[i], arr[i + 1])){ return this.randomCoordinates()}
+        }
+        return arr;
+    },
+    populate() {
+        this.ships.forEach( ship => {
+            let temp = this.randomCoordinates(ship.length);
+            for (i = 0; i < temp.length; i + 2){
+                this.tiles[temp[i], temp[i + 1]].isFilled = true;
+                this.tiles[temp[i], temp[i + 1]].ship = ship;
+            }
+            
+        })
+    }
+}
+
+module.exports = gameBoard;
--- scripts/index.test.js
+++ scripts/index.test.js
@@ -1,8 +1,8 @@
 const ship = require('./index');
 
 test('initially returns an empty ship object of a certain length', () => {
-    expect(ship(1).length).toBe(1);
-    expect(ship(1).hitCount).toBe(0);
+    expect(ship(2).length).toBe(2);
+    expect(ship(2).hitCount).toBe(0);
 });
 
 test('handles attacks', () => {
@@ -17,3 +17,32 @@ test('handles attacks', () => {
     expect(aShip.isSunk()).toBe(true);
     expect(bShip.isSunk()).toBe(false);
 });
+
+const gameBoard = require('./index');
+test('creates blank 8x8 game board', () => {
+    const board = gameBoard.createTiles();
+    expect(typeof gameBoard.tiles[0][0]).toBe({isFilled: false});
+    expect(typeof gameBoard.tiles[8][8]).toEqual(undefined);
+});
+
+test('places proper amount of ships on tiles', () => {
+    const boardFill = gameBoard.createTiles();
+    boardFill.populate();
+    let count = 0;
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            if (boardFill.tiles[i][j].isFilled === true){
+                count++
+            }
+        }
+    }
+    expect(boardFill.ships.length).toBe(10);
+    expect(count).toBe(30);
+});
+    // place ships at specific coordinates by calling the ship factory function
+    // receiveAttack() 
+    //     - accepts coordinates
+    //     - if a ship is hit, send the ‘hit’ function to the correct ship
+    //     -  else records the coordinates of missed shot
+    // missed property tracks missed attacks
+    // allSunk() returns T/F if all ships are sunk
